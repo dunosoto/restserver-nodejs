5e26f58eb36a9e84f22ab237411db854f0ec6874
Daniel Uno <danielunosoto@gmail.com>
2022-03-09 19:38:35 -0400

    structure of rest server with express

---
+++ models/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+class Server {
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    //middlewares
+    this.middlewares();
+
+    //routes
+    this.routes();
+
+  }
+
+  middlewares() {
+    this.app.use(express.static('public'));
+  }
+
+  routes() {
+    this.app.get('/', (req, res) => {
+      res.send('Hello World');
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log('Servidor en el puerto', this.port);
+    });
+  }
+}
+
+module.exports = Server;
